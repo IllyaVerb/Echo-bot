5967b24b83fa037ab23b581244bb0fe40a589d93
Illya Verbovskyi <illyaverb@gmail.com>
2020-06-30 19:33:30 +0300

    Worked commit

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
-import config
+#import config
 import telebot
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot('663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY')#config.token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
+#if __name__ == '__main__':
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling()
