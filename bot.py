be1e6e2681f371f4463a0f5a4dd5ab9fef520bcd
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2020-06-30 15:37:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.polling()
