87bbec6cc8cb3051a772d03aa5cd56a619628c9f
Illya Verbovskyi <illyaverb@gmail.com>
2020-06-30 21:34:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,7 @@
 import config
 import telebot
-import os
-from flask import Flask, request
 
 bot = telebot.TeleBot(config.token)
-
-server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -15,19 +11,6 @@ def start_message(message):
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
-@server.route('/' + config.token, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://testbot2202.herokuapp.com/' + config.token)
-    return "!", 200
-
 if __name__ == '__main__':
-    #bot.remove_webhook()
-    #bot.polling()
-    #server.debug = True
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.remove_webhook()
+    bot.polling()
