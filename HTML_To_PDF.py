7134e24ed8d8658bd85b2909dc160c89a6271645
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2021-06-03 02:43:16 +0300

    Update HTML_To_PDF.py
    Change html to pdf to pdfkit.

--- HTML_To_PDF.py
+++ HTML_To_PDF.py
@@ -1,21 +1,23 @@
 import os, time
+import pdfkit
 
 class HTML_To_PDF:
     def __init__(self, input_html, output_pdf):
         path_to_file = os.getcwd()
-        name_of_file = output_pdf
-        html_file = os.getcwd() + "/" + input_html  
-        page_to_open = "file:///" + html_file
+        #name_of_file = output_pdf
+        html_file = path_to_file + "/" + input_html  
+        #page_to_open = "file:///" + html_file
 
-        command_to_run = '{0} \
-                            --headless \
-                            --no-sandbox \
-                            --no-first-run \
-                            --disable-gpu \
-                            --print-to-pdf="{1}\{2}" "{3}"'\
-                            .format(os.getenv('GOOGLE_CHROME_BIN'), path_to_file, name_of_file, page_to_open)
-        print('launch:'+command_to_run)
+        pdfkit.from_file(html_file,  path_to_file + "/" + output_pdf)
+        #command_to_run = '{0} \
+        #                    --headless \
+        #                    --no-sandbox \
+        #                    --no-first-run \
+        #                    --disable-gpu \
+        #                    --print-to-pdf="{1}\{2}" "{3}"'\
+        #                    .format(os.getenv('GOOGLE_CHROME_BIN'), path_to_file, name_of_file, page_to_open)
+        #print('launch:'+command_to_run)
 
-        os.system(command_to_run)
-        time.sleep(3)
+        #os.system(command_to_run)
+        time.sleep(1)
         
