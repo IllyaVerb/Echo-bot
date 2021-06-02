5ea51e33656ef9699fefdaffe211d6dced50c84e
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2021-06-02 23:44:33 +0300

    Update HTML_To_PDF.py
    Try to fix "Print to PDF is disabled when remote debugging is enabled." error.

--- HTML_To_PDF.py
+++ HTML_To_PDF.py
@@ -7,7 +7,7 @@ class HTML_To_PDF:
         html_file = os.getcwd() + "/" + input_html  
         page_to_open = "file:///" + html_file
 
-        command_to_run = 'google-chrome --headless --print-to-pdf="{0}\{1}" "{2}"'.format(path_to_file, name_of_file, page_to_open)
+        command_to_run = 'google-chrome --headless --no-sandbox --print-to-pdf-no-header --print-to-pdf="{0}\{1}" "{2}"'.format(path_to_file, name_of_file, page_to_open)
         print('launch:'+command_to_run)
 
         os.system(command_to_run)
