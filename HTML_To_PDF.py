1ba578c1060679da54f8c035c38923a9da241697
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2021-06-03 03:21:41 +0300

    Update HTML_To_PDF.py

--- HTML_To_PDF.py
+++ HTML_To_PDF.py
@@ -2,14 +2,26 @@ import os, time
 import pdfkit
 
 class HTML_To_PDF:
+    def _get_pdfkit_config():
+     """wkhtmltopdf lives and functions differently depending on Windows or Linux. We
+      need to support both since we develop on windows but deploy on Heroku.
+
+     Returns:
+         A pdfkit configuration
+     """
+     if platform.system() == 'Windows':
+         return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
+     else:
+         WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip()
+         return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+    
     def __init__(self, input_html, output_pdf):
         path_to_file = os.getcwd()
         #name_of_file = output_pdf
         html_file = path_to_file + "/" + input_html  
         #page_to_open = "file:///" + html_file
 
-        pdfkit.configuration()
-        pdfkit.from_file(html_file,  path_to_file + "/" + output_pdf)
+        pdfkit.from_file(html_file,  path_to_file + "/" + output_pdf, configuration=_get_pdfkit_config())
         #command_to_run = '{0} \
         #                    --headless \
         #                    --no-sandbox \
