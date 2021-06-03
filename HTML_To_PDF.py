761285730528f887f87169cbd5243df184a396b4
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2021-06-03 11:31:40 +0300

    Update HTML_To_PDF.py

--- HTML_To_PDF.py
+++ HTML_To_PDF.py
@@ -8,10 +8,10 @@ class HTML_To_PDF:
         html_file = path_to_file + "/" + input_html  
         #page_to_open = "file:///" + html_file
         
-        if platform.system() == ‘Windows’:
-            pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get(‘WKHTMLTOPDF_PATH’, ‘C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe’))
+        if platform.system() == 'Windows':
+            pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'))
         else:
-            WKHTMLTOPDF_CMD = subprocess.Popen([‘which’, os.environ.get(‘WKHTMLTOPDF_PATH’, ‘/app/bin/wkhtmltopdf’)],\
+            WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/app/bin/wkhtmltopdf')],\
                 stdout=subprocess.PIPE).communicate()[0].strip()
             pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
